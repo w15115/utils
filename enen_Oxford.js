d0592908640ebcf21edd7a2be236c1611391c4b5
w15115 <trongphuit@gmail.com>
2022-06-02 09:41:28 +0700

    Update enen_Oxford.js

--- enen_Oxford.js
+++ enen_Oxford.js
@@ -116,7 +116,7 @@ class encn_Oxford {
                     if (def.means && def.means.length > 0) {
                         let pos = def.part || def.part_name || '';
                         pos = pos ? `<span class="pos simple">${pos}</span>` : '';
-                        definition += `<li class="ec">${pos}<span class="ec_chn">${def.means.join()}</span></li>`;
+                       
                     }
                 definition += '</ul>';
                 let css = `<style>
